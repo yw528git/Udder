01af4893b21bd325b7a95a7cc157cee7befcdb6a
Marc <marc.leclercq@mac.com>
2019-08-11 08:27:22 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Marc Leclercq on 11/08/2019.
+//  Copyright © 2019 Marc Leclercq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
